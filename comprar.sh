5326590bdc909b11c1d1f58ad8c25ec2cecd9685
Rui Simões <santossimoes.ruimiguel@gmail.com>
2021-11-01 21:56:34 +0000

    all code base

--- comprar.sh
+++ comprar.sh
@@ -9,4 +9,19 @@ read modeloAutComp
 echo -n "Insira a matrícula do automóvel (Ex: 22FG02) : "
 read matriculaAutComp
 
+echo -n "Insira a o ano de fabrico (Ex: 1999) : "
+read anoFrabrico
 
+echo -n "Insira o preço de compra do automóvel (Ex: 5000)"
+read preco
+
+echo -n "Insira a data de compra (Ex: 1999 12 23)"
+read dataCompra
+
+echo -n "Insira o custo de restauto (Ex 2499)"
+read custoRestauro
+
+echo -n "Insira a valorização do automóvel restaurado (Ex 15000) "
+read valor
+
+echo $marcaAutComp : $modeloAutComp : $matriculaAutComp : $anoFrabrico : $preco : $dataCompra : $custoRestauro : $valor >> carrosComprados.txt 
